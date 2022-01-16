10d89c966e3143d95d3f3499db4f48b6139f3632
Davide Galilei <davidegalilei2018@gmail.com>
2022-01-16 19:53:36 +0100

    Bot is working

--- src/database.nim
+++ src/database.nim
@@ -11,6 +11,7 @@ type
   Chat* {.tableName: "chats".} = ref object of Model
     chatId* {.unique.}: int64
     enabled*: bool
+    percentage*: int
 
   Message* {.tableName: "messages".} = ref object of Model
     sender*: User
@@ -27,16 +28,34 @@ proc getUser*(conn: DbConn, userId: int64): User =
   new result
   conn.select(result, "users.userId = ?", userId)
 
+proc getChat*(conn: DbConn, chatId: int64): Chat =
+  new result
+  conn.select(result, "chats.chatId = ?", chatId)
+
 proc addUser*(conn: DbConn, user: User): User =
   var user = user
   conn.insert user
   return conn.getUser(user.userId)
+
+const DEFAULT_TRIGGER_PERCENTAGE = 30#%
+proc addChat*(conn: DbConn, chat: Chat): Chat =
+  var chat = chat
+  if chat.percentage == 0:
+    chat.percentage = DEFAULT_TRIGGER_PERCENTAGE
+  conn.insert chat
+  return conn.getChat(chat.chatId)
 
 proc getOrInsert*(conn: DbConn, user: User): User =
   try:
     return conn.getUser(user.userId)
   except NotFoundError:
     return conn.addUser(user) 
+
+proc getOrInsert*(conn: DbConn, chat: Chat): Chat =
+  try:
+    return conn.getChat(chat.chatId)
+  except NotFoundError:
+    return conn.addChat(chat) 
 
 proc updateOrCreate*(conn: DbConn, user: User): User =
   result = conn.getOrInsert(user)
@@ -45,9 +64,29 @@ proc updateOrCreate*(conn: DbConn, user: User): User =
   conn.update(user)
   result = user
 
+proc updateOrCreate*(conn: DbConn, chat: Chat): Chat =
+  result = conn.getOrInsert(chat)
+  var chat = chat
+  chat.id = result.id
+  conn.update(chat)
+  result = chat
+
+proc addMessage*(conn: DbConn, message: Message) =
+  var message = message
+  conn.insert message
+
+proc getLatestMessages*(conn: DbConn, chatId: int64, count: int = 500): seq[Message] =
+  result = @[Message(sender: User(), chat: Chat())]
+  conn.select(result, "chatId = ? ORDER BY messages.id DESC LIMIT ?", chatId, count)
+
 proc setAdmin*(conn: DbConn, userId: int64, admin: bool = true): User =
   result = conn.updateOrCreate(User(userId: userId, admin: admin))
 
+proc setEnabled*(conn: DbConn, chatId: int64, enabled: bool = true): Chat =
+  result = conn.updateOrCreate(Chat(chatId: chatId, enabled: enabled))
+
+proc getCount*(conn: DbConn, model: typedesc): int64 =
+  return conn.count(model)
 
 when isMainModule:
   import os
--- src/markinim.nim
+++ src/markinim.nim
@@ -1,13 +1,55 @@
-import std/[asyncdispatch, logging, options, os, times, strutils, strformat, tables]
-import telebot, norm / [model, sqlite]
+import std/[asyncdispatch, logging, options, os, times, strutils, strformat, tables, random, sets, parsecfg, sequtils]
+import telebot, norm / [model, sqlite], nimkov / generator
 import ./database
 
 var L = newConsoleLogger(fmtStr="$levelname, [$time] ")
 addHandler(L)
 
-const API_KEY = slurp("../secret.key").strip()
-var conn: DbConn
+var
+  conn: DbConn
+  botUsername: string
+  admins: HashSet[int64]
+  markovs: Table[int64, MarkovGenerator]
+  adminsCache: Table[(int64, int64), (int64, bool)] # (chatId, userId): (unixtime, isAdmin) cache
+  antiFlood: Table[int64, seq[int64]]
+
+const
+  ANTIFLOOD_SECONDS = 30
+  ANTIFLOOD_RATE = 3
+
 let t = epochTime()
+
+proc isFlood(chatId: int64, rate: int = ANTIFLOOD_RATE, seconds: int = ANTIFLOOD_SECONDS): bool =
+  let time = getTime().toUnix
+  if chatId notin antiFlood:
+    antiflood[chatId] = @[time]
+  else:
+    antiFlood[chatId].add(time)
+
+  antiflood[chatId] = antiflood[chatId].filterIt(time - it < seconds)
+  return len(antiflood[chatId]) >= rate
+
+proc cleanerWorker {.async.} =
+  while true:
+    let time = getTime().toUnix
+    for chatId in antiFlood.keys:
+      antiFlood[chatId] = antiflood[chatId].filterIt(time - it < ANTIFLOOD_SECONDS)
+    await sleepAsync(30)
+
+proc isAdminInGroup(bot: Telebot, chatId: int64, userId: int64): Future[bool] {.async.} =
+  let time = getTime().toUnix
+  if (chatId, userId) in adminsCache:
+    let (timestamp, isAdmin) = adminsCache[(chatId, userId)]
+    if not time - timestamp > (5 * 60): # result is valid for five minutes
+      return isAdmin
+
+  try:
+    let member = await bot.getChatMember(chatId = $chatId, userId = userId.int)
+    result = member.status == "creator" or member.status == "administrator"
+  except Exception:
+    result = false
+  
+  adminsCache[(chatId, userId)] = (time, result)
 
 
 type KeyboardInterrupt = ref object of CatchableError
@@ -16,36 +58,94 @@ proc handler() {.noconv.} =
 setControlCHook(handler)
 
 
-proc handleCommand(bot: Telebot, update: Update, user: database.User) {.async.} =
+proc handleCommand(bot: Telebot, update: Update, command: string, args: seq[string]) {.async.} =
   let
     message = update.message.get
-    text = message.text.get
-    splitted = text.split()
-    command = splitted[0].strip(chars = {'/'}, trailing = false)
-    args = if len(splitted) > 1: splitted[1 .. ^1] else: @[]
+    senderId = message.fromUser.get().id
 
   case command:
   of "start":
-    if message.chat.id != user.userId: # /start only works in PMs
+    if message.chat.id != senderId: # /start only works in PMs
       return
     discard await bot.sendMessage(message.chat.id, "gtfo")
-  of "admin", "unadmin":
+  of "admin", "unadmin", "remadmin":
     if len(args) < 1:
       return
-    elif not user.admin:
+    elif senderId notin admins:
       discard await bot.sendMessage(message.chat.id, &"You are not allowed to perform this command")
       return
 
     try:
       let userId = parseBiggestInt(args[0])
       discard conn.setAdmin(userId = userId, admin = (command == "admin"))
+      
+      if command == "admin":
+        admins.incl(senderId)
+      else:
+        admins.excl(senderId)
 
       discard await bot.sendMessage(message.chat.id,
         if command == "admin": &"Successfully promoted [{userId}](tg://user?id={userId})"
         else: &"Successfully demoted [{userId}](tg://user?id={userId})",
         parseMode = "markdown")
-    except Exception:
-      discard await bot.sendMessage(message.chat.id, &"An error occurred: <code>{getCurrentExceptionMsg()}</code>", parseMode = "html")
+    except Exception as error:
+      discard await bot.sendMessage(message.chat.id, &"An error occurred: <code>{$typeof(error)}: {getCurrentExceptionMsg()}</code>", parseMode = "html")
+  of "count":
+    if senderId notin admins:
+      discard await bot.sendMessage(message.chat.id, &"You are not allowed to perform this command")
+      return
+    discard await bot.sendMessage(message.chat.id,
+      &"*Users*: `{conn.getCount(database.User)}`\n*Chats*: `{conn.getCount(database.Chat)}`",
+      parseMode = "markdown")
+  of "enable", "disable":
+    if message.chat.kind.endswith("group") and not await bot.isAdminInGroup(chatId = message.chat.id, userId = senderId):
+      discard await bot.sendMessage(message.chat.id, "You are not allowed to perform this command")
+      return
+
+    discard conn.setEnabled(message.chat.id, enabled = (command == "enable"))
+
+    discard await bot.sendMessage(message.chat.id,
+      if command == "enable": "Successfully enabled learning in this chat"
+      else: "Successfully disabled learning in this chat"
+    )
+  of "percentage":
+    if message.chat.kind.endswith("group") and not await bot.isAdminInGroup(chatId = message.chat.id, userId = senderId):
+      discard await bot.sendMessage(message.chat.id, "You are not allowed to perform this command")
+      return
+    elif len(args) < 1:
+      discard await bot.sendMessage(message.chat.id, "This command needs an argument. Example: `/percentage 40` (default: `30`)", parseMode = "markdown")
+      return
+
+    try:
+      let percentage = parseInt(args[0].strip(chars = Whitespace + {'%'}))
+
+      if percentage notin 1 .. 100:
+        discard await bot.sendMessage(message.chat.id, "Percentage must be a number between 1 and 100")
+
+      var chat = conn.getChat(chatId = message.chat.id)
+      chat.percentage = percentage
+      conn.update(chat)
+
+      discard await bot.sendMessage(message.chat.id, "Percentage has been successfully updated")
+    except ValueError:
+      discard await bot.sendMessage(message.chat.id, "The value you inserted is not a number")
+  of "markov":
+    let enabled = conn.getChat(chatId = message.chat.id).enabled
+    if not enabled:
+      discard bot.sendMessage(message.chat.id, "Learning is not enabled in this chat. Enable it with /enable (for groups: admins only)")
+      return
+    
+    if not markovs.hasKey(message.chat.id):
+      markovs[message.chat.id] = newMarkov(@[])
+      for dbMessage in conn.getLatestMessages(chatId = message.chat.id):
+        if dbMessage.text != "":
+          markovs[message.chat.id].addSample(dbMessage.text)
+
+    let generated = markovs[message.chat.id].generate()
+    if generated.isSome:
+      discard await bot.sendMessage(message.chat.id, generated.get())
+    else:
+      discard await bot.sendMessage(message.chat.id, "Not enough data to generate a sentence")
 
 
 proc updateHandler(bot: Telebot, u: Update): Future[bool] {.async, gcsafe.} =
@@ -53,23 +153,77 @@ proc updateHandler(bot: Telebot, u: Update): Future[bool] {.async, gcsafe.} =
     # return true will make bot stop process other callbacks
     return true
 
-  let response = u.message.get
-  let msgUser = response.fromUser.get
-
-  let user = conn.getOrInsert(database.User(userId: msgUser.id))
+  let
+    response = u.message.get
+    msgUser = response.fromUser.get
+    chatId = response.chat.id
 
   if response.text.isSome:
-    let text = response.text.get
+    var
+      text = response.text.get
+      splitted = text.split()
+      command = splitted[0].strip(chars = {'/'}, trailing = false)
+      args = if len(splitted) > 1: splitted[1 .. ^1] else: @[]
+
     if text.startswith('/'):
-      await bot.handleCommand(u, user)
+      if msgUser.id notin admins and isFlood(chatId):
+        return true
+
+      if '@' in text:
+        let splittedCommand = text.split('@')
+        if splittedCommand[^1].toLower() != botUsername:
+          return true
+        command = splittedCommand[0]
+      await handleCommand(bot, u, command, args)
       return true
 
+    let chat = conn.getOrInsert(database.Chat(chatId: chatId))
+    if not chat.enabled:
+      return
+
+    if not markovs.hasKeyOrPut(chatId, newMarkov(@[text])):
+      for message in conn.getLatestMessages(chatId = chatId):
+        if message.text != "":
+          markovs[chatId].addSample(message.text)
+    else:
+      markovs[chatId].addSample(text)
+
+    let user = conn.getOrInsert(database.User(userId: msgUser.id))
+    conn.addMessage(database.Message(text: text, sender: user, chat: chat))
+
+    if rand(0 .. 100) <= chat.percentage:
+      let generated = markovs[chatId].generate()
+      if generated.isSome:
+        discard await bot.sendMessage(chatId, generated.get())
+
+
+proc main {.async.} =
+  let
+    config = loadConfig(currentSourcePath.parentDir / "../secret.ini")
+    botToken = config.getSectionValue("config", "token")
+    admin = config.getSectionValue("config", "admin")
+
+  conn = initDatabase("markov.db")
+
+  if admin != "":
+    admins.incl(conn.setAdmin(userId = parseBiggestInt(admin)).userId)
+
+  let bot = newTeleBot(botToken)
+  botUsername = (await bot.getMe()).username.get().toLower()
+
+  asyncCheck cleanerWorker()
+
+  bot.onUpdate(updateHandler)
+  await bot.pollAsync(timeout = 300, clean = true)
+
 when isMainModule:
+  when defined(windows):
+    if CompileDate != now().format("yyyy-MM-dd"):
+      echo "You can't run this on windows after a day"
+      quit(1)
+
   try:
-    conn = initDatabase("markov.db")
-    let bot = newTeleBot(API_KEY)
-    bot.onUpdate(updateHandler)
-    bot.poll(timeout=300)
+    waitFor main()
   except KeyboardInterrupt:
-    echo "\nQuitting...\nProgram has run for ", formatFloat(epochTime() - t, precision = 0), " seconds."
+    echo "\nQuitting...\nProgram has run for ", toInt(epochTime() - t), " seconds."
     quit(0)
