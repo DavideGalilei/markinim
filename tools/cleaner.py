b53a151661d29398c1123cd84b3a7464fe8f543c
Davide Galilei <davidegalilei2018@gmail.com>
2022-07-14 13:32:43 +0200

    Added utility to clean the database

---
+++ tools/cleaner.py
@@ -0,0 +1,23 @@
+# Small utility script to clean the redundant messages
+# from the database, and vacuum it, to make it smaller
+# and faster.
+
+import sqlite3
+
+from pathlib import Path
+
+file = Path(__file__).parent / "markov.db"
+
+with sqlite3.connect(file) as conn:
+    cur = conn.cursor()
+    for session_id in cur.execute("SELECT id FROM sessions").fetchall():
+        cur.execute("DELETE FROM messages WHERE messages.session = ? AND id NOT IN (SELECT id FROM messages WHERE messages.session = ? ORDER BY id DESC LIMIT 1500)", session_id * 2)
+        print(session_id)
+
+    conn.commit()
+
+    print("vacuum")
+    conn.execute("VACUUM")
+    conn.commit()
+
+print("done")
