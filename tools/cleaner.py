7ccd1a91b84794f8b5331d328b3152296b8ffc99
Davide Galilei <davidegalilei2018@gmail.com>
2022-12-04 12:20:58 +0100

    Make the cleaner utility blazing fast and optimized

--- tools/cleaner.py
+++ tools/cleaner.py
@@ -5,14 +5,30 @@
 import sqlite3
 
 from pathlib import Path
+from collections import defaultdict
 
 file = Path(__file__).parent / "data" / "markov.db"
 
+KEEP_LAST = 1500  # messages
+
 with sqlite3.connect(file) as conn:
+    count = conn.cursor().execute("SELECT COUNT(1) FROM messages").fetchone()[0]
+    print(f"Fetching {count} messages...")
+    all_messages = conn.cursor().execute("SELECT session, id FROM messages").fetchall()
+    session_messages = defaultdict(set)
+
+    for message in all_messages:
+        session, mid = message
+        session_messages[session].add(mid)
+
+    print("Cleaning messages...")
+
     cur = conn.cursor()
-    for session_id in cur.execute("SELECT id FROM sessions").fetchall():
-        cur.execute("DELETE FROM messages WHERE messages.session = ? AND id NOT IN (SELECT id FROM messages WHERE messages.session = ? ORDER BY id DESC LIMIT 1500)", session_id * 2)
-        print(session_id)
+    for session, messages in session_messages.items():
+        to_delete = sorted(messages, reverse=True)[KEEP_LAST:]
+        print(f"Deleting {len(to_delete)} messages in session={session}...")
+        for mid in to_delete:
+            cur.execute("DELETE FROM messages WHERE id = ?", (mid,))
 
     conn.commit()
 
