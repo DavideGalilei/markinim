04ec3fd503f496b3a221f4e0a48a6caa58942b99
Davide Galilei <davidegalilei2018@gmail.com>
2023-08-23 15:14:52 +0200

    Backup system updates (removed while true)

--- tools/backup_script.sh
+++ tools/backup_script.sh
@@ -17,20 +17,18 @@ TELEGRAM_ID=
 cd "$root_dir"
 mkdir -p "$backup_directory"
 
-while true; do
-    # Clean redundant data
-    python3 tools/cleaner.py
+# Clean redundant data
+python3 tools/cleaner.py
 
-    sqlite3 "$root_dir/markov.db" ".backup '$backup_directory/$backup_filename'"
+sqlite3 "$root_dir/markov.db" ".backup '$backup_directory/$backup_filename'"
 
-    if [ -n "$TELEGRAM_ID" ]; then
-        source "$root_dir/.env" || true
-        # if BOT_TOKEN env var exists
-        if [ -n "$BOT_TOKEN" ]; then
-            # curl telegram botapi
-            curl -s -X POST "https://api.telegram.org/bot$BOT_TOKEN/sendMessage" -d "chat_id=$TELEGRAM_ID" -d "text=Backup started at $(date)"
-        fi
+if [ -n "$TELEGRAM_ID" ]; then
+    source "$root_dir/.env" || true
+    # if BOT_TOKEN env var exists
+    if [ -n "$BOT_TOKEN" ]; then
+        # curl telegram botapi
+        curl -s -X POST "https://api.telegram.org/bot$BOT_TOKEN/sendMessage" -d "chat_id=$TELEGRAM_ID" -d "text=Backup started at $(date)"
     fi
+fi
 
-    # syncthing will sync the backup in background
-done
+# syncthing will sync the backup in background
